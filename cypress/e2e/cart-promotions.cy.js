0a11a6cf6e568026b91cc1a947971fe0a4155796
joaoabrantesuchoa <joao.abrantesuchoa@gmail.com>
2023-10-08 17:02:11 -0300

    test: add the three first tests and adjust before each

--- cypress/e2e/cart-promotions.cy.js
+++ cypress/e2e/cart-promotions.cy.js
@@ -4,7 +4,16 @@ describe('cart promotions', () => {
     cy.get('[id="_username"]').type('sylius');
     cy.get('[id="_password"]').type('sylius');
     cy.get('.primary').click();
+
+    /* cy.clickInFirst('a[href="/admin/promotions/"]');
+
+    cy.get('input.bulk-select-checkbox').last().check();
+
+    cy.get('button.ui.red.labeled.icon.button').first().click();
+
+    cy.get('div.ui.green.ok.inverted.button#confirmation-button').click();*/
   });
+
   // Remove .only and implement others test cases!
   it('increase percentage of christmas promotion', () => {
     // Click in cart promotions in side menu
@@ -24,11 +33,70 @@ describe('cart promotions', () => {
     cy.get('body').should('contain', 'Promotion has been successfully updated.');
   });
 
-  it('create new promotion', () => {});
+  it('create new promotion', () => {
+    cy.clickInFirst('a[href="/admin/promotions/"]');
 
-  it('create new promotion cupom based', () => {});
+    //Create new promotion and type the code and name.
+    cy.get('a.ui.labeled.icon.button.primary').click();
 
-  it('filter cupom based promotions', () => {});
+    cy.get('[name="sylius_promotion[code]"]').type('new_promotion_code');
+
+    cy.get('[name="sylius_promotion[name]"]').type('New promotion name');
+
+    cy.get('button.ui.labeled.icon.primary.button').click();
+
+    //Return to promotion page
+    cy.clickInFirst('a[href="/admin/promotions/"]');
+
+    cy.get('body').should('contain', 'new_promotion_code');
+    cy.get('body').should('contain', 'New promotion name');
+
+    //Remove recent created promotion
+    cy.get('input.bulk-select-checkbox').last().check();
+
+    cy.get('button.ui.red.labeled.icon.button').first().click();
+
+    cy.get('div.ui.green.ok.inverted.button#confirmation-button').click();
+  });
+
+  it('create new promotion cupom based', () => {
+    cy.clickInFirst('a[href="/admin/promotions/"]');
+
+    //Create new promotion and type the code and name.
+    cy.get('a.ui.labeled.icon.button.primary').click();
+
+    cy.get('[name="sylius_promotion[code]"]').type('new_promotion_code');
+
+    cy.get('[name="sylius_promotion[name]"]').type('New promotion name');
+
+    //CUPOM based checkbox
+    cy.get('label[for="sylius_promotion_couponBased"]').click();
+
+    cy.get('button.ui.labeled.icon.primary.button').click();
+
+    //Return to promotion page
+    cy.clickInFirst('a[href="/admin/promotions/"]');
+
+    cy.get('span.ui.basic.label').contains('Yes').first().click();
+
+    //Remove recent created promotion
+    cy.get('input.bulk-select-checkbox').last().check();
+
+    cy.get('button.ui.red.labeled.icon.button').first().click();
+
+    cy.get('div.ui.green.ok.inverted.button#confirmation-button').click();
+  });
+
+  it.only('filter cupom based promotions', () => {
+    cy.clickInFirst('a[href="/admin/promotions/"]');
+
+    cy.get('#criteria_couponBased').select('true');
+
+    cy.get('*[class^="ui blue labeled icon button"]').click();
+
+    cy.get('body').should('contain', 'Christmas');
+    cy.get('body').should('not.contain', 'New Year');
+  });
 
   it('filter not cupom based promotions', () => {});
 
